6a4e44d29c49a5eb159114f539bdcb04d92f148f
Paul Traina <bulk+github@pst.org>
2020-08-13 22:57:13 -0700

    Bump version to 1.0.1

--- adif_merge/__init__.py
+++ adif_merge/__init__.py
@@ -37,7 +37,7 @@ from datetime import datetime, timedelta
 
 import adif_io
 
-__VERSION__ = "1.0.0"
+__VERSION__ = "1.0.1"
 
 # merge any calls in the same band and same mode within 90 seconds
 MERGE_WINDOW = 90
